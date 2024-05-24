f99fdd4a103fa4620dfce2890a0d7f39d4f89c8d
0siek <114651472+0siek@users.noreply.github.com>
2024-05-25 00:05:29 +0200

    Release v1.0.8
    Działa naciśnięcie na mapie i podświetlenie w tabeli(brakuje jeszcze na odwrót)

---
+++ App.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useMemo } from "react";
+import "./App.css";
+import { DataItem, SortConfig } from "./lib/types";
+import { sortData } from "./lib/sorting";
+import MapComponent from "./components/MapComponent";
+import TableComponent from "./components/TableComponent";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
+const App: React.FC = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null);
+  const [selectedDevice, setSelectedDevice] = useState<DataItem | null>(null); // Nowy stan dla wybranego urządzenia
+
+  // Funkcja do obsługi kliknięcia urządzenia na mapie
+  const handleDeviceSelection = (device: DataItem) => {
+    setSelectedDevice(device);
+  };
+  const sortedData = useMemo(
+    () => sortData(data, sortConfig),
+    [data, sortConfig]
+  );
+
+  const requestSort = (key: keyof DataItem) => {
+    let direction: "ascending" | "descending" = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/radios");
+        // const response = await fetch("/example-data.json"); w przypadku osx (bez serwera)
+
+        const data: DataItem[] = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="p-8 flex justify-between items-center">
+        {/* Nagłówek */}
+        <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+          <span className="pb-2 border-b px-5">Konsola Operatorska</span>
+        </h1>
+        {/* Przełącznik trybu */}
+        <ModeToggle />
+      </div>
+      {/* Mapa */}
+      <MapComponent data={data} onDeviceSelection={handleDeviceSelection} />
+      {/* Tabela */}
+      <div className="p-5">
+        <TableComponent
+          data={sortedData}
+          sortConfig={sortConfig}
+          requestSort={requestSort}
+          selectedDevice={selectedDevice}
+          onDeviceSelection={handleDeviceSelection}
+        />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ TableComponent.tsx
@@ -0,0 +1,314 @@
+"use client";
+
+import * as React from "react";
+import {
+  CaretSortIcon,
+  ChevronDownIcon,
+  DotsHorizontalIcon,
+} from "@radix-ui/react-icons";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DataItem, SortConfig } from "../lib/types";
+import { useState } from "react";
+
+const columns: ColumnDef<DataItem>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "Name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("Name")}</div>,
+  },
+  {
+    accessorKey: "Type",
+    header: "Type",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("Type")}</div>,
+  },
+  {
+    accessorKey: "SerialNumber",
+    header: "Serial Number",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("SerialNumber")}</div>
+    ),
+  },
+  {
+    accessorKey: "BatteryLevel",
+    header: () => <div className="text-right">Battery Level</div>,
+    cell: ({ row }) => {
+      const batteryLevel = parseFloat(row.getValue("BatteryLevel"));
+
+      return <div className="text-right font-medium">{batteryLevel}</div>;
+    },
+  },
+  {
+    accessorKey: "Strength",
+    header: () => <div className="text-right">Strength</div>,
+    cell: ({ row }) => {
+      const strength = parseFloat(row.getValue("Strength"));
+
+      return <div className="text-right font-medium">{strength}</div>;
+    },
+  },
+  {
+    accessorKey: "WorkingMode",
+    header: "Working Mode",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("WorkingMode")}</div>
+    ),
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const item = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(item.Id)}
+            >
+              Copy item ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+interface TableComponentProps {
+  data: DataItem[];
+  sortConfig: SortConfig | null;
+  requestSort: (key: keyof DataItem) => void;
+  selectedDevice: DataItem | null; // Dodana definicja dla wybranego urządzenia
+  onDeviceSelection: (device: DataItem) => void; // Funkcja obsługująca zmianę wybranego urządzenia
+}
+
+const TableComponent: React.FC<TableComponentProps> = ({
+  data,
+  selectedDevice,
+  onDeviceSelection,
+}) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const handleRowClick = (device: DataItem) => {
+    if (selectedDevice !== device) {
+      onDeviceSelection(device);
+    }
+  };
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        {/* Filtr */}
+        <Input
+          placeholder="Filter items..."
+          value={(table.getColumn("Type")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("Type")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        {/* Menu rozwijane kolumn */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      {/* Tabela */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={
+                    row.original === selectedDevice ? "selected" : undefined
+                  } // Zaznaczenie wiersza odpowiadającego wybranemu urządzeniu
+                  onClick={() => handleRowClick(row.original)} // Obsługa kliknięcia na wiersz
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      {/* Licznik wybranych urządzeń i nawigacja */}
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TableComponent;
