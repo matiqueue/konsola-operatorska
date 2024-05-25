bcc0d918c181e79da087faf6683edfe914e1c52e
matiqueue <matiqueue@gmail.com>
2024-05-25 03:00:29 +0200

    Design v3

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -6,7 +6,7 @@ import "leaflet/dist/leaflet.css";
 
 interface MapComponentProps {
   data: DataItem[];
-  onDeviceSelection: (device: DataItem) => void; // Nowa funkcja do przekazania stanu wybranego urządzenia
+  onDeviceSelection: (device: DataItem) => void;
 }
 
 const MapComponent: React.FC<MapComponentProps> = ({
@@ -21,7 +21,7 @@ const MapComponent: React.FC<MapComponentProps> = ({
       map.current = L.map("mapId").setView([50.062, 19.906], 12);
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "© OpenStreetMap contributors",
+        attribution: "Wykonanie robili Szymon G, Patryk K",
       }).addTo(map.current);
     }
   }, []);
@@ -35,7 +35,7 @@ const MapComponent: React.FC<MapComponentProps> = ({
         const marker = L.marker([item.Position.Lat, item.Position.Lon], {
           icon: icon,
         }).bindPopup(`Id: ${item.Id}, Name: ${item.Name}, Type: ${item.Type}`);
-        marker.on("click", () => onDeviceSelection(item)); // Obsługa kliknięcia na markerze
+        marker.on("click", () => onDeviceSelection(item));
         markerLayer.current.addLayer(marker);
       });
 
@@ -44,10 +44,8 @@ const MapComponent: React.FC<MapComponentProps> = ({
   }, [data, onDeviceSelection]);
 
   return (
-    <div className="px-40">
-      <div className="mx-auto border-dashed border-2 border-gray-700 dark:border-gray-400 m-2 p-2 rounded">
-        <div id="mapId" className="h-96 w-full"></div>
-      </div>
+    <div className="mx-auto border-double border-2 border-gray-600 dark:border-gray-300 p-4 rounded-tr-3xl rounded-bl-3xl rounded-tl rounded-br  hover:border-solid">
+      <div id="mapId" className="h-96 w-full"></div>
     </div>
   );
 };
